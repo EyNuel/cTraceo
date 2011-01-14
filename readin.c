a5b2564c11506de20b8414d9a90ad05d806248a0
Emanuel Ey <emanuel.ey@gmail.com>
2011-01-14 18:44:38 +0000

    Added:	in"readin.c":	now reading and validating sound speed info.

--- readin.c
+++ readin.c
@@ -25,12 +25,13 @@ void	readIn(globals_t* globals, const char* filename){
 	 *		none		Values are read into "globals->settings"			*
 	 ***********************************************************************/
 
-	uint64_t	i;
+	uint64_t	i,j;
 	double		dTheta;				//DISCUSS: really necessary?
 	uint64_t	nThetas;			//used locally to make code more readable. Value is stored in settings.
 	double		theta0;				//used locally to make code more readable. Value is stored in settings.
 	double		thetaN;				//used locally to make code more readable. Value is stored in settings.
 	uint64_t	numSurfaceCoords;	//used locally to make code more readable. Value is stored in settings.
+	uint64_t	nr0, nz0;			//used locally to make code more readable. Value is stored in settings.
 	char*		tempString;
 	FILE*		infile;					//a pointer for the input file
 	infile = openFile(filename, "r");	//open file in "read" mode
@@ -68,7 +69,7 @@ void	readIn(globals_t* globals, const char* filename){
 		fatal(	"Source initial range is outside the range box!\nAborting...");
 	}
 	if( globals->settings.source.nThetas > MAX_NUM_ANGLES){
-		fatal(	"Input file: NTHETAS > MAX_NUM_ANGLES.\nAborting...");
+		fatal(	"Input file: nThetas > MAX_NUM_ANGLES.\nAborting...");
 	}
 	
 	/*	Allocate memory for the launching angles	*/
@@ -95,7 +96,7 @@ void	readIn(globals_t* globals, const char* filename){
 	
 
 	/************************************************************************
-	 * Read altimetry info:
+	 * Read and validate altimetry info:
 	 ***********************************************************************/
 	 
 	skipLine(infile);
@@ -104,12 +105,16 @@ void	readIn(globals_t* globals, const char* filename){
 	tempString = readString(infile);
 	if(strcmp(tempString,"A") == 0){
 		globals->settings.altimetry.surfaceType	= SURFACE_TYPE__ABSORVENT;
+		
 	}else if(strcmp(tempString,"E") == 0){
 		globals->settings.altimetry.surfaceType	= SURFACE_TYPE__ELASTIC;
+		
 	}else if(strcmp(tempString,"R") == 0){
 		globals->settings.altimetry.surfaceType	= SURFACE_TYPE__RIGID;
+		
 	}else if(strcmp(tempString,"V") == 0){
 		globals->settings.altimetry.surfaceType	= SURFACE_TYPE__VACUUM;
+		
 	}else{
 		fatal("Input file: unknown surface type.\nAborting...");
 	}
@@ -119,10 +124,12 @@ void	readIn(globals_t* globals, const char* filename){
 	tempString = readString(infile);
 	if(strcmp(tempString,"H") == 0){
 		globals->settings.altimetry.surfacePropertyType	= SURFACE_PROPERTY_TYPE__HOMOGENEOUS;
+		
 	}else if(strcmp(tempString,"N") == 0){
 		globals->settings.altimetry.surfacePropertyType	= SURFACE_PROPERTY_TYPE__NON_HOMOGENEOUS;
+		
 	}else{
-		fatal("Input file: unknown surface properties.\nAborting...");
+		fatal("Input file: unknown surface property type.\nAborting...");
 	}
 	free(tempString);
 
@@ -130,14 +137,19 @@ void	readIn(globals_t* globals, const char* filename){
 	tempString = readString(infile);
 	if(strcmp(tempString,"FL") == 0){
 		globals->settings.altimetry.surfaceInterpolation	= SURFACE_INTERPOLATION__FLAT;
+		
 	}else if(strcmp(tempString,"SL") == 0){
 		globals->settings.altimetry.surfaceInterpolation	= SURFACE_INTERPOLATION__SLOPED;
+		
 	}else if(strcmp(tempString,"2P") == 0){
 		globals->settings.altimetry.surfaceInterpolation	= SURFACE_INTERPOLATION__2P;
+		
 	}else if(strcmp(tempString,"3P") == 0){
 		globals->settings.altimetry.surfaceInterpolation	= SURFACE_INTERPOLATION__3P;
+		
 	}else if(strcmp(tempString,"4P") == 0){
 		globals->settings.altimetry.surfaceInterpolation	= SURFACE_INTERPOLATION__4P;
+		
 	}else{
 		fatal("Input file: unknown surface interpolation type.\nAborting...");
 	}
@@ -147,14 +159,19 @@ void	readIn(globals_t* globals, const char* filename){
 	tempString = readString(infile);
 	if(strcmp(tempString,"F") == 0){
 		globals->settings.altimetry.surfaceAttenUnits	= SURFACE_ATTEN_UNITS__dBperkHz;
+		
 	}else if(strcmp(tempString,"M") == 0){
 		globals->settings.altimetry.surfaceAttenUnits	= SURFACE_ATTEN_UNITS__dBperMeter;
+		
 	}else if(strcmp(tempString,"N") == 0){
 		globals->settings.altimetry.surfaceAttenUnits	= SURFACE_ATTEN_UNITS__dBperNeper;
+		
 	}else if(strcmp(tempString,"Q") == 0){
 		globals->settings.altimetry.surfaceAttenUnits	= SURFACE_ATTEN_UNITS__qFactor;
+		
 	}else if(strcmp(tempString,"W") == 0){
 		globals->settings.altimetry.surfaceAttenUnits	= SURFACE_ATTEN_UNITS__dBperLambda;
+		
 	}else{
 		fatal("Input file: unknown surface attenuation units.\nAborting...");
 	}
@@ -180,6 +197,7 @@ void	readIn(globals_t* globals, const char* filename){
 			globals->settings.altimetry.surfaceProperties = malloc(sizeof(interfaceProperties_t));
 			if( globals->settings.altimetry.surfaceProperties == NULL)
 				fatal("Memory allocation error.");
+				
 			globals->settings.altimetry.surfaceProperties[0].cp	= readDouble(infile);
 			globals->settings.altimetry.surfaceProperties[0].cs	= readDouble(infile);
 			globals->settings.altimetry.surfaceProperties[0].rho= readDouble(infile);
@@ -198,6 +216,7 @@ void	readIn(globals_t* globals, const char* filename){
 			globals->settings.altimetry.surfaceProperties = malloc(numSurfaceCoords*sizeof(interfaceProperties_t));
 			if( globals->settings.altimetry.surfaceProperties == NULL)
 				fatal("Memory allocation error.");
+				
 			for(i=0; i<numSurfaceCoords; i++){
 				globals->settings.altimetry.r[i] = readDouble(infile);
 				globals->settings.altimetry.z[i] = readDouble(infile);
@@ -210,177 +229,147 @@ void	readIn(globals_t* globals, const char* filename){
 			break;
 	}
 
+	/*	Validate Altimetry info		*/
+	if(numSurfaceCoords > NBDRY)
+		fatal("Input file: altimetry points > NBDRY.\nAborting...");	//TODO replace NBDRY
+
+
+	/************************************************************************
+	 * Read and validate sound speed info:
+	 ***********************************************************************/
+	/*	sound speed distribution	"cdist"		*/
+	skipLine(infile);
+	tempString = readString(infile);
+	if(strcmp(tempString,"c(z,z)") == 0){
+		globals->settings.soundSpeed.cDist	= C_DIST__PROFILE;
+	}else if(strcmp(tempString,"c(r,z)") == 0){
+		globals->settings.soundSpeed.cDist	= C_DIST__FIELD;
+	}else{
+		fatal("Input file: unknown sound speed distribution type.\nAborting...");
+	}
+	free(tempString);
+
+	/*	sound speed class	"cclass"		*/
+	tempString = readString(infile);
+	if(strcmp(tempString,"ISOV") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__ISOVELOCITY;
+		
+	}else if(strcmp(tempString,"LINP") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__LINEAR;
+		
+	}else if(strcmp(tempString,"PARP") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__PARABOLIC;
+		
+	}else if(strcmp(tempString,"EXPP") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__EXPONENTIAL;
+		
+	}else if(strcmp(tempString,"N2LP") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__N2_LINEAR;
+		
+	}else if(strcmp(tempString,"ISQP") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__INV_SQUARE;
+		
+	}else if(strcmp(tempString,"MUNK") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__MUNK;
+		
+	}else if(strcmp(tempString,"TABL") == 0){
+		globals->settings.soundSpeed.cClass	= C_CLASS__TABULATED;
+		
+	}else{
+		fatal("Input file: unknown sound class type.\nAborting...");
+	}
+	free(tempString);
+
+	/* number of points in range and depth, "nr0,nz0" */
+	nr0 = (uint64_t)readInt(infile);
+	nz0 = (uint64_t)readInt(infile);
+	globals->settings.soundSpeed.nr0 = nr0;
+	globals->settings.soundSpeed.nz0 = nz0;
+
+	//verify that number of values of soundspeed are within limits
+	if(	(globals->settings.soundSpeed.cDist == C_DIST__PROFILE) &&
+		(nz0 > NC0))
+		fatal("Sound speed profile points > NC0!\nAborting...");	//TODO replace NC0
+
+	if(	globals->settings.soundSpeed.cDist == C_DIST__FIELD){
+		if(nr0 > NC02D)
+			fatal("Sound speed field range points > NC02D.\nAborting...");	//TODO replace NC02D
+
+		if(nz0 > NC02D)
+			fatal("Sound speed field depth points > NC02D.\nAborting...");	//TODO replace NC02D
+	}
+
+	//read actual values of soundspeed profile/field:
+	switch(globals->settings.soundSpeed.cDist){
+		
+		case C_DIST__PROFILE:
+			if(globals->settings.soundSpeed.cClass != C_CLASS__TABULATED){
+				//all cClasses execept for "TABULATED" only require ( z0(0),c0(0) ) and ( z0(1), c0(1) )
+				//malloc z0 an c0:
+				globals->settings.soundSpeed.z0 = mallocDouble(2);
+				globals->settings.soundSpeed.c01d = mallocDouble(2);
+
+				//read 4 values:
+				globals->settings.soundSpeed.z0[0] = readDouble(infile);
+				globals->settings.soundSpeed.c01d[0] = readDouble(infile);
+				globals->settings.soundSpeed.z0[1] = readDouble(infile);
+				globals->settings.soundSpeed.c01d[1] = readDouble(infile);
+
+				// validate the values that were just read:
+				if(	(globals->settings.soundSpeed.cClass != C_CLASS__ISOVELOCITY) &&
+					(globals->settings.soundSpeed.cClass != C_CLASS__MUNK)){
+					
+					if(globals->settings.soundSpeed.z0[0] == globals->settings.soundSpeed.z0[1])
+						fatal("Analytical sound speed: z[1] == z[0] Only valid for Isovelocity and Munk Options!\nAborting...");
+
+					if(globals->settings.soundSpeed.c01d[0] == globals->settings.soundSpeed.c01d[1])
+						fatal("Analytical sound speed: c[1] == c[0] Only valid for Isovelocity option!\nAborting...");
+				}
+			}else if(globals->settings.soundSpeed.cClass == C_CLASS__TABULATED){
+				//malloc z0 an c0:
+				globals->settings.soundSpeed.z0 = mallocDouble(nz0);
+				globals->settings.soundSpeed.c01d = mallocDouble(nz0);	//TODO verify this is correct
+
+				//read pairs of z0 and c0
+				for(i=0; i<globals->settings.soundSpeed.nz0; i++){
+					globals->settings.soundSpeed.z0[i]	= readDouble(infile);
+					globals->settings.soundSpeed.c01d[i]= readDouble(infile);
+				}
+			}
+			break;
+		
+		case C_DIST__FIELD:
+			if(globals->settings.soundSpeed.cClass != C_CLASS__TABULATED)
+				fatal("Unknown sound speed filed type.\nAborting...");
+			
+			//malloc ranges (vector)
+			globals->settings.soundSpeed.r0 = mallocDouble(nr0);
+			//read ranges
+			for(i=0; i<nr0; i++)
+				globals->settings.soundSpeed.r0[i] = readDouble(infile);
+
+			//malloc depths (vector)
+			globals->settings.soundSpeed.z0 = mallocDouble(nz0);
+			//read depths
+			for(i=0; i<nz0; i++)
+				globals->settings.soundSpeed.z0[i] = readDouble(infile);
+
+			//malloc sound speeds (2 dim matrix)
+			globals->settings.soundSpeed.c02d = mallocDouble2D(nr0, nz0);	//mallocDouble2D(numCols, numRows)
+			//read sound speeds
+			for(j=0; j<nz0; j++){		//rows
+				for(i=0; i<nr0; i++){	//columns
+					globals->settings.soundSpeed.c02d[j][i] = readDouble(infile);
+				}
+			}
+			break;
+	}
 	if (VERBOSE)
 		printSettings(globals);
+	
 }
 /*
-       if (nati.gt.nbdry) then
 
-           write(6,*) 'altimetry points  > ',nbdry,'!'
-	   write(6,*) 'aborting calculations...'
-           stop
-           
-       end if
-c-----------------------------------------------------------------------
-       if ((atiu.ne.'F').and.(atiu.ne.'M').and.(atiu.ne.'N').
-     & and.(atiu.ne.'Q').and.(atiu.ne.'W')) then
-      
-          write(6,*) 'Surface attenuation: unknown units,'
-          write(6,*) 'aborting calculations...'
-          stop
-      
-       end if
-c-----------------------------------------------------------------------
-
-       if (aptype.eq.'H') then
-	  
-       read(inpfil,*) cpati(1),csati(1),rhoati(1),apati(1),asati(1)
-	   
-       do i = 1,nati
-          
-	  read(inpfil,*) rati(i), zati(i)
-	      
-       end do
-       
-       else if (aptype.eq.'N') then
-	        
-       do i = 1,nati
-          
-	  read(inpfil,*) rati(i),  zati(i), cpati(i), csati(i),
-     &                 rhoati(i), apati(i), asati(i)
-          
-       end do
-		
-       else
-       
-	   write(6,*) 'inpfil: Unknown surface properties,'
-           write(6,*) 'aborting calculations...'
-	   stop
-
-       end if
-
-c***********************************************************************
-c     Read sound speed info:
-c***********************************************************************
-
-       read(inpfil,*)
-       read(inpfil,*) cdist
-       read(inpfil,*) cclass
-       read(inpfil,*) nr0,nz0
-       
-       cclss = cclass(1:4)
-       
-       if ((cdist.eq.'c(z,z)').and.(nz0.gt.nc0)) then
-	
-           write(6,*) 'Sound speed profile points > ',nc0,'!'
-           write(6,*) 'aborting calculations...'
-           stop
- 
-       end if
-	     
-       if (cdist.eq.'c(r,z)') then
- 
-            if (nr0.gt.nc02d) then
-             write(6,*) 'Sound speed field range points > ',nc02d,'!'
-             write(6,*) 'aborting calculations...'
-             stop
-            end if
- 
-            if (nz0.gt.nc02d) then
-             write(6,*) 'Sound speed field depth points > ',nc02d,'!'
-             write(6,*) 'aborting calculations...'
-             stop
-            end if
- 
-       end if
-c-----------------------------------------------------------------------
-
-c      Sound speed profile case:
-
-       if (cdist.eq.'c(z,z)') then
-
-c#######################################################################
-        if ((cclss.ne.'TABL')) then
-	    
-	     read(inpfil,*) z0(1), c0(1)
-	     read(inpfil,*) z0(2), c0(2)
-	       
-	     if ((cclss.ne.'ISOV').and.(cclss.ne.'MUNK')) then
-	
- 	       if ( z0(1).eq.z0(2) ) then
- 	     	write(6,*) 'Analytical sound speed: z(2) = z(1)'
-	     	write(6,*) 'valid only for isovelocity and!'
-	     	write(6,*) 'Munk options!'
- 	     	stop
- 	       end if
-
-	       if ( c0(1).eq.c0(2) ) then
- 	     	write(6,*) 'Analytical sound speed: c(2) = c(1)'
-	     	write(6,*) 'valid only for isovelocity option!'
- 	     	stop
-	       end if
-
-	     end if
-
-c#######################################################################
-	     else if (cclss.eq.'TABL') then
-	
-	       do i = 1,nz0
- 
-	          read(inpfil,*) z0(i), c0(i)
- 
-	       end do
-
-c#######################################################################
-	     else
-	   
-	      write(6,*) 'inpfil: Unknown sound speed profile type,'
-	      write(6,*) 'aborting calculations...'
-              stop
-	    
-	     end if
-c#######################################################################
-
-c      Sound speed field on a rectangular grid:    
-
-       else if (cdist.eq.'c(r,z)') then
-
-c#######################################################################
-	    if (cclss.eq.'TABL') then
-	   
-c           Read the vector of ranges: 
-  
-	        read(inpfil,*) (r0(i),i=1,nr0)
-		   
-c           Read the vector of depths:
-	   
-	        read(inpfil,*) (z0(i),i=1,nz0)
-
-c           Read the matrix of sound speed: 
-      
- 	        do i = 1,nz0
-
-                read(inpfil,*) (c02d(i,j), j = 1,nr0)
-
- 	        end do
-
-c#######################################################################
-	       else 
-	       
-	        write(6,*) 'inpfil: Unknown sound speed field type,'
-	        write(6,*) 'aborting calculations...'
- 	        stop
-
-	       end if
-c#######################################################################
-
-       else
-       
-           write(6,*) 'inpfil: Unknown sound speed distribution,'
-	   write(6,*) 'aborting calculations...'
-           stop
-       
-       end if
 
 c***********************************************************************
 c     Read object info:
