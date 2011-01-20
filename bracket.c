c3c00d43fd08eaba16b2530cf85f0f305fbaa515
Emanuel Ey <emanuel.ey@gmail.com>
2011-01-20 13:45:50 +0000

    Added: "bracket.c": as well as test cases "testing/t_bracket.c" and test binary.

---
+++ bracket.c
@@ -0,0 +1,87 @@
+/********************************************************************************
+ *	bracket.c		 															*
+ * 	(formerly "brcket.for")														*
+ * 	Given a sequence x0 < x1 < ... <xj < ... < xn, and a number xi, such that:	*
+ * 		 x0 < xi < xn,															*
+ * 	with xi not belonging to the sequence, determine j such that:				*
+ * 		 xj < xi < x[j+1]														*
+ *																				*
+ *	Used to determine between which values to interpolate.						*
+ * 																				*
+ *	originally written in FORTRAN by:											*
+ *  					Orlando Camargo Rodriguez:								*
+ *						Copyright (C) 2010										*
+ * 						Orlando Camargo Rodriguez								*
+ *						orodrig@ualg.pt											*
+ *						Universidade do Algarve									*
+ *						Physics Department										*
+ *						Signal Processing Laboratory							*
+ *																				*
+ *	Ported to C by:		Emanuel Ey												*
+ *						emanuel.ey@gmail.com									*
+ *						Signal Processing Laboratory							*
+ *						Universidade do Algarve									*
+ *																				*
+ *	Inputs:																		*
+ * 				n:		number of elements in vector x							*
+ * 				x:		vector to be searched									*
+ * 				xi:		scalar who's bracketing elements are to be found		*
+ * 	Outputs:																	*
+ * 				i:		the index of the lower bracketing element.				*
+ * 																				*
+ *******************************************************************************/
+
+void	bracket(uintptr_t, double*, double, uintptr_t*);
+
+void	bracket(uintptr_t n, double* x, double xi, uintptr_t* i){
+	uintptr_t	ia,im,ib;
+
+	ia = 0;
+	ib = n-1;
+
+	//if xi is outside the interval [ x[0], x[n-1] ], quit
+	if( (xi < x[0]) || (xi > x[n-1])){
+		fatal("Oops, something is rotten in the state of Denmark...");
+	}else{
+		while( ib-ia > 1){
+			//If xi is inside the interval [x(1) x(n)] just divide it by half until ib - ia = 1:
+			im = (ia+ib)/2;		//TODO check what happens with non-integer results
+			if( (xi >= x[ia] ) && ( xi < x[im])){
+				ib = im;
+			}else{
+				ia = im;
+			}
+			//printf("ia: %u; ib: %u\n",ia, ib);
+		}
+		*i = im;
+		//for even numbers, the returned index has to be adjusted
+		if( (n & 0x01) == 0 ){
+			*i-=1;
+		}
+	}
+}
+
+/*
+
+500	im = (ia+ib)/2
+
+	   if ((ib-ia).gt.1) then
+	   
+		  if ((xi.ge.x(ia)).and.(xi.lt.x(im))) then
+		   ib = im
+		  else
+		   ia = im
+		  end if
+	   
+		  goto 500
+	   
+	   end if
+
+	   i = im
+
+	   return
+
+c=======================================================================
+
+	   end
+*/
---
+++ testing/t_bracket.c
@@ -0,0 +1,73 @@
+/*
+ * This code contains some testing cases for bracket.c
+ * Compiled with: clang -march=i686 -m32 -lm -o t_bracket.exe t_bracket.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../tools.c"
+#include "../bracket.c"
+#include <math.h>
+
+int main(void){
+
+	double*		xVector = NULL;	//shall contain a sorted vector of numbers
+	double 		xi;				//an element who's "bracketing" elements are to be found
+	uint32_t	i, j, n;
+	
+	//a sequence with an even number of elements
+	n = 6;
+	xVector = mallocDouble(n);
+	xVector[0] = 0.1365;
+	xVector[1] = 0.1730;
+	xVector[2] = 0.2844;
+	xVector[3] = 0.5155;
+	xVector[4] = 0.6946;
+	xVector[5] = 0.8385;
+	xi = 0.52;				//returned index should be "3"
+	
+	printf("\nx:");
+	for(i=0; i<n; i++){
+		printf(" %5.4lf",xVector[i]);
+	}
+	printf("\nxi: %5.4lf\tindex: ",xi);
+	
+	bracket(n, xVector, xi, &j);
+	printf("%u\n",j);
+	free(xVector);
+	
+	/* a sequence with an odd number of elements */
+	n = 7;
+	xVector = mallocDouble(n);
+	xVector[0] = 0.1365;
+	xVector[1] = 0.1730;
+	xVector[2] = 0.2844;
+	xVector[3] = 0.5155;
+	xVector[4] = 0.6946;
+	xVector[5] = 0.8385;
+	xVector[6] = 0.9385;
+	xi = 0.8000;				//returned index should be "4"
+	
+	printf("\nx:");
+	for(i=0; i<n; i++){
+		printf(" %5.4lf",xVector[i]);
+	}
+	printf("\nxi: %5.4lf\tindex: ",xi);
+	
+	bracket(n, xVector, xi, &j);
+	printf("%u\n",j);
+	
+	/* a case in wich xi belongs to the sequence	*/
+	xi = 0.1730;
+	printf("\nx:");
+	for(i=0; i<n; i++){
+		printf(" %5.4lf",xVector[i]);
+	}
+	printf("\nxi: %5.4lf\tindex: ",xi);
+	
+	bracket(n, xVector, xi, &j);
+	printf("%u\n",j);
+	free(xVector);
+
+	exit(EXIT_SUCCESS);
+}
