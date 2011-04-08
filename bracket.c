be50d6fb1034d09d55b40d884aef4d3efe83fc6c
Emanuel Ey <emanuel.ey@gmail.com>
2011-04-08 16:55:35 +0100

    change: bracket() now returns an integer indicating if bracketing was successfull or not.

--- bracket.c
+++ bracket.c
@@ -24,19 +24,24 @@
  *						Universidade do Algarve									*
  *																				*
  *	Inputs:																		*
- * 				n:		number of elements in vector x							*
- * 				x:		vector to be searched									*
- * 				xi:		scalar who's bracketing elements are to be found		*
- * 	Outputs:																	*
- * 				i:		the index of the lower bracketing element.				*
- * 																				*
+ *				n:		number of elements in vector x							*
+ *				x:		vector to be searched									*
+ *				xi:		scalar who's bracketing elements are to be found		*
+ *	Outputs:																	*
+ *				i:		the index of the lower bracketing element.				*
+ *																				*
+ *	Return Value:																*
+ *				0:		Bounding error, don't use the returned index "i" -will	*
+ * 						likely result in a segfault if you use it.				*
+ *				1:		Success. Retuned index "i" is valid and safe to use.	*
+ *																				*
  *******************************************************************************/
 
 #pragma once
 
-void	bracket(uintptr_t, double*, double, uintptr_t*);
+uintptr_t	bracket(uintptr_t, double*, double, uintptr_t*);
 
-void	bracket(uintptr_t n, double* x, double xi, uintptr_t* i){
+uintptr_t	bracket(uintptr_t n, double* x, double xi, uintptr_t* i){
 	DEBUG(5, "Entering bracket().\n");
 	uintptr_t	ia,im,ib;
 
@@ -48,13 +53,18 @@ DEBUG(5,"xi: %e\n",		xi);
 DEBUG(5,"x[0]: %e\n",	x[0]);
 DEBUG(5,"[n-1]: %e\n",	x[n-1]);
 
-	//if xi is outside the interval [ x[0], x[n-1] ], quit
+	//if xi is outside the interval [ x[0], x[n-1] ], return zero
 	if( (xi < x[0]) || (xi > x[n-1])){
+		/* This was the old behaviour: when the index was out of bounds,
+		 * bracket would quit with an error.
 		printf("n: %u\n",		(uint32_t)n);
 		printf("xi: %e\n",		xi);
 		printf("x[0]: %e\n",	x[0]);
 		printf("[n-1]: %e\n",	x[n-1]);
-		fatal("Bounding error in bracket().\nAborting.");
+		fatal("Bounding error in bracket().\nAborting."); //TODO uncomment!
+		*/
+		//now it will return 0, thus indicating a bounding error
+		return 0;
 	}else{
 		while( ib-ia > 1){
 			//If xi is inside the interval [x(1) x(n)] just divide it by half until ib - ia = 1:
@@ -67,6 +77,7 @@ DEBUG(5,"[n-1]: %e\n",	x[n-1]);
 			//printf("ia:%u; im:%u; ib:%u\n",ia, im, ib);
 		}
 		*i = ia;
+		return 1;
 	}
 	DEBUG(5, "Leaving bracket().\n");
 }
