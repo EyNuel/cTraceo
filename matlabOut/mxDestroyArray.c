90a44c3b4905e2a729ac41f9f963ad6501f6fb7d
Emanuel Ey <emanuel.ey@gmail.com>
2011-11-11 14:13:22 +0000

    bugfix: "matlabout/": now correctly freeing memory of structure arrays with empty fields.

--- matlabOut/mxDestroyArray.c
+++ matlabOut/mxDestroyArray.c
@@ -8,24 +8,26 @@ void mxDestroyArray(mxArray* inArray){
 	/*
 	 * Free's all memory alocated to an mxArray.
 	 */
-	 if (inArray->pr_double != NULL){
-		free(inArray->pr_double);
-	 }
-	 if (inArray->pr_char != NULL){
-		free(inArray->pr_char);
-	 }
-	 if (inArray->numericType == mxCOMPLEX && inArray->pi_double != NULL){
-		free(inArray->pi_double);
-	 }
-	 //if this is a structure, free its children
-	 if (inArray->isStruct){
-		if (inArray->fieldNames != NULL){
-			for (uintptr_t i=0; i<inArray->nFields; i++){
-				if (inArray->fieldNames[i] != NULL){
-					free((void*)inArray->fieldNames[i]);
+	if (inArray != NULL){
+		if (inArray->pr_double != NULL){
+			free(inArray->pr_double);
+		}
+		if (inArray->pr_char != NULL){
+			free(inArray->pr_char);
+		}
+		if (inArray->numericType == mxCOMPLEX && inArray->pi_double != NULL){
+			free(inArray->pi_double);
+		}
+		//if this is a structure, free its children
+		if (inArray->isStruct){
+			if (inArray->fieldNames != NULL){
+				for (uintptr_t i=0; i<inArray->nFields; i++){
+					if (inArray->fieldNames[i] != NULL){
+						free((void*)inArray->fieldNames[i]);
+					}
+					//mxDestroyArray(&inArray->field[i]);
 				}
-				//mxDestroyArray(&inArray->field[i]);
 			}
 		}
-	 }
+	}
 }
