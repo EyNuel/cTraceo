b1f31098f10f9c8b6800a9ad93cea94ff4676e7b
Emanuel Ey <emanuel.ey@gmail.com>
2011-11-11 11:41:02 +0000

    added: "matlabOut/": appendin to matfiles is now supported.

--- matOpen.c
+++ matOpen.c
@@ -24,14 +24,19 @@ MATFile* matOpen(const char* filename, const char* openMode){
 			//verify file was opened successfully
 			if(outfile == NULL) {
 				fatal("Error while opening file.\n");
-				exit(EXIT_FAILURE);		//this is redundant but avoids "control may reach end of non-void function" warning
 			}
 			
 			//write matlab header to file:
 			writeMatfileHeader(outfile, MATLAB_HEADER_TEXT);
 			break;
 		case 'u':
-			fatal("Opening MATFiles in 'update' mode is not yet supported.");	//TODO
+			//open file in mode "append+binary"
+			outfile = fopen(filename, "ab");
+			
+			//verify file was opened successfully
+			if(outfile == NULL) {
+				fatal("Error while opening file.\n");
+			}
 			break;
 		default:
 			fatal("Opening MATFiles in 'read' mode is not supported [nor planned].");
