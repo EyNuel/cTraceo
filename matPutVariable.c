7328ba842282239f6961e5403e303a40c341f103
Emanuel Ey <emanuel.ey@gmail.com>
2011-11-15 13:42:59 +0000

    Squashed 'matlabOut/' content from commit 252ca36
    git-subtree-dir: matlabOut
    git-subtree-split: 252ca36ece682c16349b2e60b697adc2cba01573

---
+++ matPutVariable.c
@@ -0,0 +1,27 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include "matlabOut.h"
+#include "writeArray.c"
+#include "writeStructArray.c"
+
+uintptr_t matPutVariable(MATFile* outfile, const char* arrayName, mxArray* inArray);
+
+uintptr_t matPutVariable(MATFile* outfile, const char* arrayName, mxArray* inArray){
+	/*
+	 * Writes an mxArray to a matfile;
+	 * TODO add support for structures.
+	 * TODO actually verify successfull write instead of siomply returning 0
+	 */
+	
+	calcArraySize(inArray);
+	//printf("After calcArraySize: inArray->nBytes = %lu \n", inArray->nBytes);
+	
+	if (inArray->isStruct == false){
+		//assume that if it isn't struct, its a matrix
+		writeArray(outfile, arrayName, inArray);
+	}else{
+		writeStructArray(outfile, arrayName, inArray);
+	}
+	return 0;
+}
