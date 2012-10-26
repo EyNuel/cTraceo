f0d3da3af5e8bd0671a4d01ce6288bbb036a2a08
Emanuel Ey <emanuel.ey@gmail.com>
2012-10-26 22:38:16 +0100

    improved logging output.

---
+++ source/logOptions.c
@@ -0,0 +1,64 @@
+/****************************************************************************************
+ * logOptions.c                                                                         *
+ * Writes program options to log file.                                                  *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ *
+ * Website:                                                                             *
+ *          https://github.com/EyNuel/cTraceo/wiki                                      *
+ *                                                                                      *
+ * License: This file is part of the cTraceo Raytracing Model and is released under the *
+ *          Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License  *
+ *          http://creativecommons.org/licenses/by-nc-sa/3.0/                           *
+ *                                                                                      *
+ * NOTE:    cTraceo is research code under active development.                          *
+ *          The code may contain bugs and updates are possible in the future.           *
+ *                                                                                      *
+ * Written for project SENSOCEAN by:                                                    *
+ *          Emanuel Ey                                                                  *
+ *          emanuel.ey@gmail.com                                                        *
+ *          Copyright (C) 2011, 2012                                                    *
+ *          Signal Processing Laboratory                                                *
+ *          Universidade do Algarve                                                     *
+ *                                                                                      *
+ * cTraceo is the C port of the FORTRAN 77 TRACEO code written by:                      *
+ *          Orlando Camargo Rodriguez:                                                  *
+ *          Copyright (C) 2010                                                          *
+ *          Orlando Camargo Rodriguez                                                   *
+ *          orodrig@ualg.pt                                                             *
+ *          Universidade do Algarve                                                     *
+ *          Physics Department                                                          *
+ *          Signal Processing Laboratory                                                *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ *
+ *  Inputs (when using "explicit" version):                                             *
+ *          settings:   Pointer to the settings structure.                              *
+ *                                                                                      *
+ *  Outputs:                                                                            *
+ *          None (writes to log file on disk).                                          *
+ *                                                                                      *
+ *  Return Value:                                                                       *
+ *          None                                                                        *
+ *                                                                                      *
+ ****************************************************************************************/
+
+void logOptions(settings_t* settings);
+
+void logOptions(settings_t* settings){
+    
+    if(settings->options.saveSSP == true){
+        LOG("Option '--ssp' enabled; saving sound speed profile to 'ssp.mat'\n");
+    }
+    
+    if(settings->options.killBackscatteredRays == true){
+        LOG("Option '--killBackscatteredRays' enabled; terminating any backscattered rays.\n");
+    }
+    
+    if(settings->options.writeHeader == false){
+        LOG("Option '--noHeader' enabled; not displaying model's header on stdout.\n");
+    }
+}
+    
+    
+    
+    
+    
